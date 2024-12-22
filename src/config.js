28f946d422382aeb15b9f0025bdd76d186a0762a
Subhransu-sekhar-1355 <sekhar1355@gmail.com>
2024-12-22 16:06:47 +0530

    final push

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     s3: {
         REGION: "us-east-1",
-        BUCKET: "notes-api-upload-sekhar",
+        BUCKET: "notes-api-uploads-sekhar",
     },
     apiGateway: {
         REGION: "us-east-1",
@@ -9,7 +9,8 @@ const config = {
     },
     cognito: {
         REGION: "us-east-1",
-        USER_POOL_ID: "7mf2849a3q6rpsvkepctk7t7sp",
+        USER_POOL_ID: "us-east-1_4HwWEEUqK",
+        APP_CLIENT_ID: "7mf2849a3q6rpsvkepctk7t7sp",
         IDENTITY_POOL_ID: "us-east-1:a5d433b2-7ff1-4157-821b-9334c785735b",
     },
 };
